602dc84e75b1c989872107bf9a58e2fc5808656e
Lan Nguyen <nguyenlan13@gmail.com>
2019-07-07 18:48:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,11 +49,10 @@ class Student
   end
   
   def self.new_from_db(row)
-    student 
     student.id = row[0]
     student.name =  row[1]
     student.grade = row[2]
-    student = self.new(name, grade)
+    student = self.new(id, name, grade)
   end
   
   def self.find_by_name(name)
